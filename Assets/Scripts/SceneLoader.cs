7de8c20967d503ff8f84fd946f0fb03f01f81afc
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-02-16 09:12:55 -0300

    added a splash screen with a singleton pattern music player

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float splashScreenTime = 2f;
+    int currentScene;
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        if (currentScene == 0)
+        {
+            Invoke("LoadNextScene", splashScreenTime);
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
