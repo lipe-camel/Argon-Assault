f14c21173b53ad288f73555741689bbbaf9761f5
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-03-28 12:40:42 -0300

    Now the playe rotates dynamically

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,29 +1,55 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Player))]
 public class PlayerMovement : MonoBehaviour
 {
-    //
-    [SerializeField] float movementSpeed = 10f;
-    [SerializeField] float yValue, xValue;
+    //CONFIG STATS
+    [Header("Screen Movement")]
+    [SerializeField] float xValue;
+    [SerializeField] float yValue;
+    [Header("Movement")]
+    [SerializeField] float movementSpeed = 85f;
+
+    [Header("Screen Rotation Correction")]
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float positionYawFactor = 1.5f;
+    [Header("Rotation")]
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float controlRollFactor = -10f;
 
     // CACHED REFERENCES
     Player player;
+    Transform playerTransform;
 
     public void CustomStart()
     {
         player = GetComponent<Player>();
+        playerTransform = player.playerModel.transform;
     }
 
-
-    public void Move(float xFactor, float yFactor)
+    public void ProcessPlayerMovement(float xThrow, float yThrow)
     {
-        float deltaX = xFactor * Time.deltaTime * movementSpeed;
-        float deltaY = yFactor * Time.deltaTime * movementSpeed;
+        Move(xThrow, yThrow);
+        ProcessRotation(xThrow, yThrow);
+    }
+    private void Move(float xThrow, float yThrow)
+    {
+        float deltaX = xThrow * Time.deltaTime * movementSpeed;
+        float deltaY = yThrow * Time.deltaTime * movementSpeed;
 
-        var newXPos = Mathf.Clamp(player.playerModel.transform.localPosition.x + deltaX, -xValue, xValue);
-        var newYPos = Mathf.Clamp(player.playerModel.transform.localPosition.y + deltaY, -yValue, yValue);
+        float newXPos = Mathf.Clamp(playerTransform.localPosition.x + deltaX, -xValue, xValue);
+        float newYPos = Mathf.Clamp(playerTransform.localPosition.y + deltaY, -yValue, yValue);
 
-        player.playerModel.transform.localPosition = new Vector3(newXPos, newYPos);
+        playerTransform.localPosition = new Vector3(newXPos, newYPos);
+    }
+
+    private void ProcessRotation(float xThrow, float yThrow)
+    {
+        float pitch = playerTransform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw =   playerTransform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        playerTransform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
