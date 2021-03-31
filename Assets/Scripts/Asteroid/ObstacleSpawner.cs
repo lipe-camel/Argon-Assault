a0724921c7df228898565870778847412e12b647
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-03-31 18:58:39 -0300

    now obstacles spawn scattered in a Vector2 transform

---
+++ Assets/Scripts/Asteroid/ObstacleSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    //CONFIG PARAMS
+    [SerializeField] GameObject obstacle;
+    [SerializeField] float xSpawnLimit = 19f, ySpawnLimit = 12f;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+
+    //STATES
+    bool spawn = true;
+
+
+    private IEnumerator Start()
+    {   
+
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnObstacle();
+            //apply -z velocity
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        float rndmX = Random.Range(-xSpawnLimit, xSpawnLimit);
+        float rndmY = Random.Range(-ySpawnLimit, ySpawnLimit);
+        Vector3 randomPos = new Vector3(rndmX, rndmY, transform.position.z);
+
+        GameObject spawnedObstacle = Instantiate(obstacle, randomPos, transform.rotation);
+        spawnedObstacle.transform.parent = transform;
+    }
+}
