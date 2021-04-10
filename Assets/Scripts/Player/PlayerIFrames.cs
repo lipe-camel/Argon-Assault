d25d01d8d6174d33ca0e54ef43af5072714541a4
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-04-10 12:14:34 -0300

    sucessfuly implemented iframes

--- Assets/Scripts/Player/PlayerIFrames.cs
+++ Assets/Scripts/Player/PlayerIFrames.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 public class PlayerIFrames : MonoBehaviour
 {
     //CONFIG PARAMS
-    [SerializeField] int numberOfFlashes = 4;
-    [SerializeField] float flashDuration = 0.1f;
 
     //CACHED INTERNAL REFERENCES
     Player player;
@@ -15,26 +13,4 @@ public class PlayerIFrames : MonoBehaviour
         player = GetComponent<Player>();
     }
 
-    internal void ManageIframes()
-    {
-        int temp = 0;
-        player.boxCollider.enabled = false;
-        Debug.Log($"{this.name} colliders are enabled? {player.boxCollider.enabled}");
-        while(temp < numberOfFlashes)
-        {
-            StartCoroutine(ToggleVisibility(false));        
-            StartCoroutine(ToggleVisibility(true));
-            temp++;
-        }
-        player.boxCollider.enabled = true;
-        Debug.Log($"{this.name} colliders are enabled? {player.boxCollider.enabled}");
-    }
-
-    private IEnumerator ToggleVisibility(bool isMeshEnabled)
-    {
-        player.meshRenderer.enabled = isMeshEnabled;
-        Debug.Log($"Mesh renderes are enabled? {isMeshEnabled}");
-
-        yield return new WaitForSeconds(flashDuration);
-    }
 }
