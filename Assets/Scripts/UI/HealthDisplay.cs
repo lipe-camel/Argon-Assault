9875531979ee071b7963aaafd14650cbcebaca11
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-04-22 18:32:05 -0300

    made a flashing healthbar

--- Assets/Scripts/UI/HealthDisplay.cs
+++ Assets/Scripts/UI/HealthDisplay.cs
@@ -1,17 +1,44 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour
 {
-    //Slider slider;
+    //CONFIG PARAMS
+    [SerializeField] Color32 c1, c2, c3, c4;
+    [SerializeField] float frameRate = 12f;
 
-    //private void Start()
-    //{
-    //    slider = GetComponent<Slider>();
-    //}
+    //CACHED COMPONENT REFERENCES
+    Slider slider;
+    RawImage fillColor;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        fillColor = GetComponentInChildren<RawImage>();
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(FlashColors());
+    }
+
+    private IEnumerator FlashColors()
+    {
+        while (true)
+        {
+            fillColor.color = c1;
+            yield return new WaitForSeconds(1 / frameRate);
+            fillColor.color = c2;
+            yield return new WaitForSeconds(1 / frameRate);
+            fillColor.color = c3;
+            yield return new WaitForSeconds(1 / frameRate);
+            fillColor.color = c4;
+            yield return new WaitForSeconds(1 / frameRate);
+        }
+    }
 
     public void UpdateDisplay(float health)
     {
-        GetComponent<Slider>().value = health;
+        slider.value = health;
     }
 }
