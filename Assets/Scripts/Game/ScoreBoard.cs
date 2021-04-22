ff20ffbfa1aa7c3d1f6100fe4d9683b9c10f6441
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-04-22 14:53:22 -0300

    created the effect when reaching score milestones

--- Assets/Scripts/Game/ScoreBoard.cs
+++ Assets/Scripts/Game/ScoreBoard.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class ScoreBoard : MonoBehaviour
 {
     //STATS
     int currentScore;
+
+    //CACHED COMPONENT REFERENCES
+    AudioSource milestoneSFX;
 
     //CACHED EXTERNAL REFERENCES
     [SerializeField] ScoreDisplay scoreDisplay, finalScoreDisplay;
@@ -11,7 +15,7 @@ public class ScoreBoard : MonoBehaviour
     private void Start()
     {
         scoreDisplay.CustomStart();
-
+        milestoneSFX = GetComponent<AudioSource>();
         ResetScore();
     }
 
@@ -22,21 +26,16 @@ public class ScoreBoard : MonoBehaviour
         if (scoreDisplay)
         {
             scoreDisplay.UpdateScore(currentScore);
-        }
-    }
-
-    public void SubtractToScore(int score)
-    {
-        currentScore -= score;
-        if (scoreDisplay)
-        {
-            scoreDisplay.UpdateScore(currentScore);
+            if(currentScore >= 1000)
+            {
+                milestoneSFX.Play();
+                StartCoroutine(scoreDisplay.TwinkleScore());
+            }
         }
     }
 
     public void ShowFinalScore()
     {
-        Debug.Log($"my score is {currentScore}");
         finalScoreDisplay.UpdateScore(currentScore);
     }
 
--- Assets/Scripts/UI/ScoreDisplay.cs
+++ Assets/Scripts/UI/ScoreDisplay.cs
@@ -1,18 +1,45 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour
 {
+    //CONFIG PARAMS
+    [SerializeField] Color32 c1, c2, c3, c4;
+    [SerializeField] float frameRate = 12f;
+    [SerializeField] int numberOfFlashes= 4;
+
+    //STATE
+    Color32 defaultColor;
+
     //CACHED INTERNAL REFERENCES
     Text ScoreText;
 
     public void CustomStart()
     {
         ScoreText = GetComponent<Text>();
+        defaultColor = ScoreText.color;
     }
 
     public void UpdateScore(int score)
     {
         GetComponent<Text>().text = score.ToString();
+    }
+
+    public IEnumerator TwinkleScore()
+    {
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            ScoreText.color = c1;
+            yield return new WaitForSeconds(1 / frameRate);
+            ScoreText.color = c2;
+            yield return new WaitForSeconds(1 / frameRate);
+            ScoreText.color = c3;
+            yield return new WaitForSeconds(1 / frameRate);
+            ScoreText.color = c4;
+            yield return new WaitForSeconds(1 / frameRate);
+
+        }
+        ScoreText.color = defaultColor;
     }
 }
